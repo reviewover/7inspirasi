a96b31838fa8ecf6cb4e88c7ecc5527f8a89a8c1
reviewover <reviewoverproject@gmail.com>
2014-10-13 04:36:50 +0700

    Deploy

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'db_host' => 'aa1q2deyv0v5diw.cohgsvdszfxp.ap-southeast-1.rds.amazonaws.com',
-  'db_name' => '7inspirasi',
+  'db_host' => 'rds-7inspirasi.cohgsvdszfxp.ap-southeast-1.rds.amazonaws.com',
+  'db_name' => 'db7inspirasi',
   'db_user' => 'dbuser',
   'db_password' => 'dbpassword',
 );
