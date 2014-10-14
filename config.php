52961f1568c750cdf8a4e08102710380cb689a84
reviewover <reviewoverproject@gmail.com>
2014-10-14 09:34:13 +0700

    7itu - deploy

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'db_host' => 'rds-7inspirasi.cohgsvdszfxp.ap-southeast-1.rds.amazonaws.com',
-  'db_name' => 'db7inspirasi',
+  'db_host' => 'pitu.cohgsvdszfxp.ap-southeast-1.rds.amazonaws.com',
+  'db_name' => 'pitu',
   'db_user' => 'dbuser',
   'db_password' => 'dbpassword',
 );
